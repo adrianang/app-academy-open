db768c2eab545289c7314b536e18b4305b0a3d96
Adrian Ang <adriancbang@gmail.com>
2019-08-06 00:59:59 -0700

    Create seed_mines and render helper methods

--- ruby/git/minesweeper/board.rb
+++ ruby/git/minesweeper/board.rb
@@ -1,7 +1,45 @@
+require_relative "tile"
+
 class Board
   attr_accessor :board
 
   def initialize
     @board = Array.new(9) { Array.new(9) }
+  end
+
+  def seed_mines
+    board_size = @board.length
+    
+    seeded_mines = 0
+    until seeded_mines > (board_size ** 2 / 5)
+      random_row_coordinate = rand(board_size)
+      random_col_coordinate = rand(board_size)
+
+      if !@board[random_row_coordinate][random_col_coordinate]
+        @board[random_row_coordinate][random_col_coordinate] = Tile.new(self.board)
+        @board[random_row_coordinate][random_col_coordinate].mined = true
+        seeded_mines += 1
+      end
+    end 
+  end
+
+  def render
+    rendered_board = Array.new(9) { Array.new(9) }
+
+    (0...9).each do |row|
+      (0...9).each do |col|
+        if !@board[row][col]
+          rendered_board[row][col] = "*"
+        elsif @board[row][col].mined
+          rendered_board[row][col] = "X"
+        end
+      end
+    end
+
+    rendered_board.each do |line|
+      puts line.join(" ")
+    end
+
+    true
   end
 end
