297827048c815db1f99dd38efb60b84294613507
Adrian Ang <adriancbang@gmail.com>
2019-08-08 19:45:43 -0700

    Initialize foundation for checking tile's neighbors

--- ruby/git/minesweeper/tile.rb
+++ ruby/git/minesweeper/tile.rb
@@ -1,10 +1,17 @@
 class Tile
-  attr_accessor :mined
+  attr_accessor :mined, :neighbors, :board, :position_on_board
 
   def initialize(board)
     @mined = nil
     @flagged = false
     @revealed = false
+    @neighbors = nil
+    @board = board
+    @position_on_board = nil
+  end
+
+  def inspect
+    { 'mined' => @mined, 'flagged' => @flagged, 'revealed' => @revealed, 'neighbors' => @neighbors, 'position_on_board' => @position_on_board }.inspect
   end
 
   def reveal
@@ -14,5 +21,15 @@ class Tile
   end
 
   def neighbors
+
+  end
+
+  def neighbor_mine_count
+    mine_count = 0
+
+    (0...9).each do |row|
+    end
+
+    mine_count
   end
 end
