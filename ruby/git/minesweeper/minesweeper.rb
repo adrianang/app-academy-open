8607894f74d35fd58d9556f1deddfa9b349f450c
Adrian Ang <adriancbang@gmail.com>
2019-08-10 18:17:24 -0700

    Add user input and action methods

--- ruby/git/minesweeper/minesweeper.rb
+++ ruby/git/minesweeper/minesweeper.rb
@@ -9,12 +9,42 @@ class Minesweeper
 
   def play
     self.board.construct
-    puts "Pick a position (row, column):"
-    self.get_input
+
+    until self.game_over?
+      puts "Pick a position (row, column):"
+      pos = self.get_input_position
+
+      puts "Would you like to reveal ('r') or flag ('f') this position?"
+      action = self.get_input_action
+      if action == "r"
+        self.board[pos].reveal
+      else
+        self.board[pos].flag
+      end
+
+      self.board.render
+    end
+
+    puts "Game over!"
   end
 
-  def get_input
+  def get_input_position
     user_input = gets.chomp.split(",").map(&:to_i)
     user_input
+  end
+
+  def get_input_action
+    user_input = gets.chomp(&:to_lower)
+    
+    if !(["r", "f"]).include?(user_input[0])
+      puts "Not a valid action! Type 'r' to reveal or 'f' to flag."
+      self.get_input_action
+    end
+
+    user_input
+  end
+
+  def game_over?
+    
   end
 end
