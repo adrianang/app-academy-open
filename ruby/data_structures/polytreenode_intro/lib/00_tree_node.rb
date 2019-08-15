d83ef79897d43d9adc8f0f926943945dbdf5aff6
Adrian Ang <adriancbang@gmail.com>
2019-08-14 19:04:40 -0700

    Wrote add_child and remove_child methods

--- ruby/data_structures/polytreenode_intro/lib/00_tree_node.rb
+++ ruby/data_structures/polytreenode_intro/lib/00_tree_node.rb
@@ -27,4 +27,13 @@ class PolyTreeNode
       new_parent.children << self if !new_parent.children.include?(self)
     end
   end
+
+  def add_child(child_node)
+    child_node.parent = self
+  end
+
+  def remove_child(child_node)
+    child_node.parent = nil
+    raise "Not a child of this node!" if !self.children.include?(child_node)
+  end
 end
