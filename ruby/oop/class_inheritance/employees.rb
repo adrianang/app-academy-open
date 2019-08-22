bb72a7ae87ced8d7f83d0ca2300786ea29b8d03e
Adrian Ang <adriancbang@gmail.com>
2019-08-21 20:37:27 -0700

    Write Employee class and Manger subclass

--- ruby/oop/class_inheritance/employees.rb
+++ ruby/oop/class_inheritance/employees.rb
@@ -1,8 +1,42 @@
 class Employee
+  attr_accessor :name, :title, :salary, :boss
+
   def initialize(name, title, salary, boss)
     @name = name
     @title = title
     @salary = salary
     @boss = boss
+  end
+
+  def bonus(multiplier)
+    bonus = @salary * multiplier
+    bonus
+  end
+end
+
+class Manager < Employee
+  attr_accessor :employees
+
+  def initialize(name, title, salary, boss)
+    super(name, title, salary, boss)
+    @employees = []
+  end
+
+  def bonus(multiplier)
+    total_salaries = 0
+
+    queue = [self]
+    until queue.empty?
+      manager = queue.shift
+      manager.employees.each do |employee|
+        if employee.instance_variable_defined?(:@employees)
+          queue << employee
+        end
+
+        total_salaries += employee.salary
+      end
+    end
+
+    total_salaries * multiplier
   end
 end
