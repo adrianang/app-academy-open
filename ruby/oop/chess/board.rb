e0b05b6ad0c6d4d9459fcb84db4ace4aad471a47
Adrian Ang <adriancbang@gmail.com>
2019-08-23 20:16:21 -0700

    Implement move_piece method

--- ruby/oop/chess/board.rb
+++ ruby/oop/chess/board.rb
@@ -4,6 +4,36 @@ class Board
   attr_accessor :board
 
   def initialize
-    @board = Array.new(8) { Array.new(8) { Piece.new } }
+    @board = Array.new(8) { Array.new(8) { nil } }
+    self.initialize_pieces
   end
+
+  def initialize_pieces
+    @board[0] = @board[0].map { |space| space = Piece.new }
+    @board[1] = @board[1].map { |space| space = Piece.new }
+    @board[-2] = @board[-2].map { |space| space = Piece.new }
+    @board[-1] = @board[-1].map { |space| space = Piece.new }
+  end
+
+  def [](pos)
+    row, col = pos
+    @board[row][col]
+  end
+
+  def []=(pos, value)
+    row, col = pos
+    @board[row][col] = value
+  end 
+
+  def move_piece(start_pos, end_pos)
+    raise PositionError.new "There is no piece at #{start_pos}." if self[start_pos].nil?
+    raise PositionError.new "The piece cannot move to #{end_pos}." if !self[end_pos].nil?
+
+    self[end_pos] = self[start_pos]
+    self[start_pos] = nil
+    true
+  end
+end
+
+class PositionError < StandardError
 end
