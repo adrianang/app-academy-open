02374060377eaa719d14831fa81f119124ae42ac
Adrian Ang <adriancbang@gmail.com>
2019-08-25 19:53:36 -0700

    Create Bishop piece subclass

--- ruby/oop/chess/piece.rb
+++ ruby/oop/chess/piece.rb
@@ -8,6 +8,14 @@ module Slidable
     if self.move_dirs == self.horizontal_dirs
       (0...8).each { |col| moves << [self.pos[0], col] if (col != self.pos[1]) && self.board[[self.pos[0], col]].nil? }
       (0...8).each { |row| moves << [row, self.pos[1]] if (row != self.pos[1]) && self.board[[row, self.pos[1]]].nil? }
+    elsif self.move_dirs == self.diagonal_dirs
+      (1...8).each do |move_factor|
+        ((self.pos[0] - move_factor)..(self.pos[1] + move_factor)).step(move_factor * 2).each do |row|
+          ((self.pos[0] - move_factor)..(self.pos[1] + move_factor)).step(move_factor * 2).each do |col|
+            moves << [row, col]
+          end
+        end
+      end
     end
 
     moves
@@ -52,5 +60,17 @@ class RookPiece < Piece
 
   def move_dirs
     self.horizontal_dirs
+  end
+end
+
+class BishopPiece < Piece
+  include Slidable
+
+  def initialize(board, pos)
+    super
+  end
+
+  def move_dirs
+    self.diagonal_dirs
   end
 end
