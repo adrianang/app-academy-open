b03e69d91c5460bc5ca230638553b8176fd531c1
Adrian Ang <adriancbang@gmail.com>
2019-11-05 21:52:53 -0800

    Write dominant_octopus method

--- ruby/big_o/octopus_problems/big_octopus.rb
+++ ruby/big_o/octopus_problems/big_octopus.rb
@@ -12,6 +12,37 @@ def sluggish_octo(fishes)
   longest_fish
 end
 
+def dominant_octopus(fishes)
+  merge_sort(fishes).last
+end
+
+def merge_sort(array)
+  return array if array.length <= 1
+  
+  middle = array.length / 2
+  left_subarr = merge_sort(array[0...middle])
+  right_subarr = merge_sort(array[middle..-1])
+
+  merge(left_subarr, right_subarr)
+end
+
+def merge(left_arr, right_arr)
+  sorted = []
+
+  until left_arr.empty? || right_arr.empty?
+    if left_arr[0].length > right_arr[0].length
+      sorted << right_arr.shift
+    elsif right_arr[0].length > left_arr[0].length
+      sorted << left_arr.shift
+    elsif left_arr[0].length == right_arr[0].length
+      sorted << left_arr.shift
+      sorted << right_arr.shift
+    end
+  end
+
+  sorted + left_arr + right_arr
+end
+
 def measure(&block)
   start = Time.now
   block.call
@@ -22,3 +53,6 @@ end
 
 p sluggish_octo(fish_array)
 p "sluggish_octo(): #{ measure { sluggish_octo(fish_array) } } seconds"
+
+p dominant_octopus(fish_array)
+p "dominant_octo(): #{ measure { dominant_octopus(fish_array) } } seconds"
