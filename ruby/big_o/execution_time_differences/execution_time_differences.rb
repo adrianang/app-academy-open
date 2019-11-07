2865e3e38618ee0061c792a8bde8c93bff09df33
Adrian Ang <adriancbang@gmail.com>
2019-11-06 20:27:51 -0800

    Write phase 2 (O(n)) of my_min method, add time measurement for methods

--- ruby/big_o/execution_time_differences/execution_time_differences.rb
+++ ruby/big_o/execution_time_differences/execution_time_differences.rb
@@ -6,13 +6,31 @@ def my_min1(list)
     
     list.each do |int_to_compare|
       break if int_to_compare < main_int
-      found_smallest if int_to_compare == list.last
+      found_smallest = true if int_to_compare == list.last
     end
-    
+
     return main_int if found_smallest
   end
 
   nil
 end
 
-p my_min1(int_list)
+def my_min2(list)
+  smallest = list.first
+  list.each { |integer| smallest = integer if integer < smallest }
+  smallest
+end
+
+def measure(&block)
+  start = Time.now
+  block.call
+  finish = Time.now
+  elapsed = finish - start
+  elapsed
+end
+
+p my_min1(int_list) ## The time complexity for this function is O(n^2).
+p my_min2(int_list) ## The time complexity for this funciton is O(n).
+
+p "my_min1: " + "%f" % measure { my_min1(int_list) } + " seconds"
+p "my_min1: " + "%f" % measure { my_min2(int_list) } + " seconds"
