91032bde904f6397038f622d5d148b20c6597df4
Adrian Ang <adriancbang@gmail.com>
2019-11-11 12:54:49 -0800

    Write phase 2 (O(n) time, O(1) space) for largest_contiguous_subsum method and explanation

--- ruby/big_o/execution_time_differences/execution_time_differences.rb
+++ ruby/big_o/execution_time_differences/execution_time_differences.rb
@@ -1,5 +1,7 @@
 int_list = [0, 3, 5, 4, -5, 10, 1, 90]
 subsum_ints = [5, 3, -7]
+subsum_ints2 = [2, 3, -6, 7, -6, 7]
+subsum_ints3 = [-5, -1, -3]
 
 def my_min1(list)
   list.each do |main_int|
@@ -35,6 +37,22 @@ def largest_contiguous_subsum1(list)
   subarr_sums.max  
 end
 
+def largest_contiguous_subsum2(list)
+  current_largest_subsum = list.first
+  accumulator = 0
+
+  list.each do |num|
+    accumulator = 0 if accumulator < 0
+    accumulator += num
+
+    if accumulator > current_largest_subsum
+      current_largest_subsum = accumulator
+    end
+  end
+
+  current_largest_subsum
+end
+
 def measure(&block)
   start = Time.now
   block.call
@@ -50,5 +68,9 @@ p "my_min1: " + "%f" % measure { my_min1(int_list) } + " seconds"
 p "my_min1: " + "%f" % measure { my_min2(int_list) } + " seconds"
 
 p largest_contiguous_subsum1(subsum_ints) ## The time complexity for this function is O(n^2).
+p largest_contiguous_subsum2(subsum_ints) ## The time complexity for this function is O(n) because it uses one loop that runs linearly depending on the number of inputs. It has O(1) memory because although it uses two memory spaces (current_largest_subsum and accumulator), that simplifies to a constant space notation when scaled over a long period of time.
+p largest_contiguous_subsum2(subsum_ints2)
+p largest_contiguous_subsum2(subsum_ints3)
 
 p "largest_contiguous_subsum1: " + "%f" % measure { largest_contiguous_subsum1(subsum_ints) } + " seconds"
+p "largest_contiguous_subsum2: " + "%f" % measure { largest_contiguous_subsum2(subsum_ints) } + " seconds"
