5fbf2e62aa424b2a7a9e227e125dd9f9d3e8f4da
Adrian Ang <adriancbang@gmail.com>
2019-11-14 01:01:41 -0800

    Initial commit, phase 1 (naive solution)

---
+++ ruby/big_o/windowed_max_range/windowed_max_range.rb
@@ -0,0 +1,32 @@
+require_relative "../measure"
+
+# Phase 1: Naive Solution
+def windowed_max_range(array, window_size)
+  current_max_range = nil
+
+  array.each_with_index do |ele, idx|
+    window = array[idx..idx + window_size - 1]
+
+    if window.length == window_size
+      window_min = window.min
+      window_max = window.max
+      window_max_range = window_max - window_min
+      current_max_range = window_max_range if !current_max_range || (window_max_range > current_max_range)
+    end
+  end
+
+  current_max_range
+end
+
+arr = [1, 2, 3, 5]
+arr2 = [1, 0, 2, 5, 4, 8]
+arr3 = [1, 3, 2, 5, 4, 8]
+
+puts windowed_max_range(arr, 3)
+puts windowed_max_range(arr2, 2)
+puts windowed_max_range(arr2, 3)
+puts windowed_max_range(arr2, 4)
+puts windowed_max_range(arr3, 5)
+
+p "windowed_max_range: " + "%f" % measure { windowed_max_range(arr, 3) } + " seconds"
+# The overall time complexity for this solution is O(n^2). It uses min and max methods (each O(n)) within a loop over an array (also O(n)),
