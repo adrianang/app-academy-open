fcb98844e9ae2d43e905973ea3cb78ad767e6981
Adrian Ang <adriancbang@gmail.com>
2019-11-20 21:16:22 -0800

    Override hash methods for array, string, and hash classes for Phase 2

--- ruby/lru_cache/myhashmap/lib/p02_hashing.rb
+++ ruby/lru_cache/myhashmap/lib/p02_hashing.rb
@@ -4,11 +4,25 @@ end
 
 class Array
   def hash
+    hash_to_hash = 0
+
+    self.each_with_index do |ele, idx|
+      hash_to_hash += (ele.hash + idx).hash
+    end
+
+    hash_to_hash.hash
   end
 end
 
 class String
   def hash
+    hash_to_hash = 0
+
+    self.each_char.with_index do |char, idx|
+      hash_to_hash += (char.ord.hash + idx).hash
+    end
+    
+    hash_to_hash.hash
   end
 end
 
@@ -16,6 +30,13 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    hash_to_hash = 0
+    arrayed_hash = []
+
+    self.each { |key, val| arrayed_hash << [key.to_s.ord, val] }
+    arrayed_hash.sort
+    arrayed_hash.each { |ele| hash_to_hash += ele.hash }
+
+    hash_to_hash.hash
   end
 end
