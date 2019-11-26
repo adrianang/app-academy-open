db5d0b8331806c3960a8aeb8379f236266dedd98
Adrian Ang <adriancbang@gmail.com>
2019-11-26 15:49:42 -0800

    Create LRU cache class for Phase 6

--- ruby/lru_cache/myhashmap/lib/p06_lru_cache.rb
+++ ruby/lru_cache/myhashmap/lib/p06_lru_cache.rb
@@ -14,22 +14,33 @@ class LRUCache
   end
 
   def get(key)
+    @map.include?(key) ? self.update_node!(@map[key]) : self.calc!(key)
+    self.eject! if self.count > @max
+    @map[key].val
   end
 
   def to_s
     'Map: ' + @map.to_s + '\n' + 'Store: ' + @store.to_s
   end
 
-  private
+  # private
 
   def calc!(key)
     # suggested helper method; insert an (un-cached) key
+    val = @prc.call(key)
+    @store.append(key, val)
+    @map.set(key, @store.last)
   end
 
   def update_node!(node)
     # suggested helper method; move a node to the end of the list
+    @store.append(node.key, node.val)
+    @store.remove(node.key)
+    @map.set(node.key, @store.last)
   end
 
   def eject!
+    @map.delete(@store.first.key)
+    @store.remove(@store.first.key)
   end
 end
