d0f92ec171d8d53b61be85ad661ae45c1ce6d367
Adrian Ang <adriancbang@gmail.com>
2019-11-25 20:17:17 -0800

    Create Hash Map class for Phase 5

--- ruby/lru_cache/myhashmap/lib/p05_hash_map.rb
+++ ruby/lru_cache/myhashmap/lib/p05_hash_map.rb
@@ -1,6 +1,7 @@
 require_relative 'p04_linked_list'
 
 class HashMap
+  include Enumerable
   attr_accessor :count
 
   def initialize(num_buckets = 8)
@@ -9,18 +10,35 @@ class HashMap
   end
 
   def include?(key)
+    self.bucket(key).include?(key) ? true : false
   end
 
   def set(key, val)
+    self.resize! if @count == self.num_buckets
+
+    if self.bucket(key).include?(key)
+      self.bucket(key).update(key, val)
+    else
+      self.bucket(key).append(key, val)
+      @count += 1
+    end
   end
 
   def get(key)
+    self.bucket(key).get(key)
   end
 
   def delete(key)
+    self.bucket(key).remove(key)
+    @count -= 1
   end
 
   def each
+    @store.each do |bucket|
+      bucket.each do |node|
+        yield [node.key, node.val]
+      end
+    end
   end
 
   # uncomment when you have Enumerable included
@@ -34,16 +52,30 @@ class HashMap
   alias_method :[], :get
   alias_method :[]=, :set
 
-  private
+  # private
 
   def num_buckets
     @store.length
   end
 
   def resize!
+    old_store = @store.dup
+    self.expand
+
+    old_store.each do |bucket|
+      bucket.each do |node|
+        self.set(node.key, node.val)
+      end
+    end
   end
 
   def bucket(key)
     # optional but useful; return the bucket corresponding to `key`
+    @store[key.hash % self.num_buckets]
+  end
+
+  def expand
+    @store = Array.new(self.num_buckets * 2) { LinkedList.new }
+    @count = 0
   end
 end
