bd4c86ef81ceccb541b782a34e078bb12bab2d11
Adrian Ang <adriancbang@gmail.com>
2020-04-23 03:10:26 -0700

    Add appropriate indices and constraints to tables

--- sql/active_record_2/polls_app/db/schema.rb
+++ sql/active_record_2/polls_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_20_040800) do
+ActiveRecord::Schema.define(version: 2020_04_23_064644) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2020_04_20_040800) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "question_id", null: false
+    t.index ["question_id"], name: "index_answer_choices_on_question_id"
   end
 
   create_table "polls", force: :cascade do |t|
@@ -27,6 +28,7 @@ ActiveRecord::Schema.define(version: 2020_04_20_040800) do
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_polls_on_user_id"
   end
 
   create_table "questions", force: :cascade do |t|
@@ -34,6 +36,7 @@ ActiveRecord::Schema.define(version: 2020_04_20_040800) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "poll_id", null: false
+    t.index ["poll_id"], name: "index_questions_on_poll_id"
   end
 
   create_table "responses", force: :cascade do |t|
@@ -41,12 +44,15 @@ ActiveRecord::Schema.define(version: 2020_04_20_040800) do
     t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["answer_choice_id"], name: "index_responses_on_answer_choice_id"
+    t.index ["user_id"], name: "index_responses_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
