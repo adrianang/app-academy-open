13aaa91cbb91d6c2639d673585e2173e08dafe25
Adrian Ang <adriancbang@gmail.com>
2020-04-25 04:37:55 -0700

    Add not_duplicate_response custom validation method

--- sql/active_record_2/polls_app/app/models/response.rb
+++ sql/active_record_2/polls_app/app/models/response.rb
@@ -1,6 +1,7 @@
 class Response < ApplicationRecord
   validates :answer_choice_id, presence: true
   validates :user_id, presence: true
+  validate :not_duplicate_response
 
   belongs_to :answer_choice,
     primary_key: :id,
@@ -16,5 +17,15 @@ class Response < ApplicationRecord
 
   def sibling_responses
     self.question.responses.where.not(id: self.id)
+  end
+
+  def respondent_already_answered?
+    self.sibling_responses.exists?(user_id: self.respondent.id)
+  end
+
+  def not_duplicate_response
+    if self.respondent_already_answered?
+      errors[:base] << "Respondent has already answered"
+    end
   end
 end
