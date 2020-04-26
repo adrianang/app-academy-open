480fcb724c4a45348e3966c8979d1523b12e32eb
Adrian Ang <adriancbang@gmail.com>
2020-04-25 21:40:47 -0700

    Create Question#results

--- sql/active_record_2/polls_app/app/models/question.rb
+++ sql/active_record_2/polls_app/app/models/question.rb
@@ -13,4 +13,34 @@ class Question < ApplicationRecord
     class_name: :AnswerChoice
 
   has_many :responses, through: :answer_choices, source: :responses
+
+  def results
+    self.answer_choices.select(:text)
+        .group(:text)
+        .left_outer_joins(:responses)
+        .count("responses.id")
+  end
+
+#   The following methods are here to compare two ways to get poll results, for learning purposes only.
+#   def n_plus_one_results
+#     results = self.answer_choices
+    
+#     results_counts = {}
+#     results.each do |answer_choice|
+#       results_counts[answer_choice.text] = answer_choice.responses.length
+#     end
+
+#     results_counts
+#   end
+
+#   def includes_results
+#     results = self.answer_choices.includes(:responses)
+
+#     results_counts = {}
+#     results.each do |answer_choice|
+#       results_counts[answer_choice.text] = answer_choice.responses.length
+#     end
+
+#     results_counts
+#   end
 end
