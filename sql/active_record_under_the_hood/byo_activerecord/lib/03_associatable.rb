6758e1878da2983f03add115b6d649a2ad73cdd4
Adrian Ang <adriancbang@gmail.com>
2020-05-11 21:19:09 -0700

    Complete Phase IVa: storing AssocOptions

--- sql/active_record_under_the_hood/byo_activerecord/lib/03_associatable.rb
+++ sql/active_record_under_the_hood/byo_activerecord/lib/03_associatable.rb
@@ -41,9 +41,11 @@ end
 module Associatable
   # Phase IIIb
   def belongs_to(name, options = {})
-    options = BelongsToOptions.new(name, options)
+    self.assoc_options[name] = BelongsToOptions.new(name, options)
     
     define_method(name) do
+      options = self.assoc_options[name]
+
       foreign_key_value = send(options.foreign_key)
       model_class = options.model_class
       
@@ -52,9 +54,11 @@ module Associatable
   end
 
   def has_many(name, options = {})
-    options = HasManyOptions.new(name, self.name, options)
+    self.assoc_options[name] = HasManyOptions.new(name, self.name, options)
 
     define_method(name) do
+      options = self.assoc_options[name]
+
       foreign_key_value = send(options.primary_key)
       model_class = options.model_class
 
@@ -64,6 +68,8 @@ module Associatable
 
   def assoc_options
     # Wait to implement this in Phase IVa. Modify `belongs_to`, too.
+    @assoc_options ||= {}
+    @assoc_options
   end
 end
 
