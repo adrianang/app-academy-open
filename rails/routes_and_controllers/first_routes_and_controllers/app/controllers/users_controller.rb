d4cdd5876d70be17833b64dde1182bafa1d59d17
Adrian Ang <adriancbang@gmail.com>
2020-05-15 02:26:00 -0700

    Create User model, add functionality to create user through API

--- rails/routes_and_controllers/first_routes_and_controllers/app/controllers/users_controller.rb
+++ rails/routes_and_controllers/first_routes_and_controllers/app/controllers/users_controller.rb
@@ -1,13 +1,24 @@
 class UsersController < ApplicationController
   def index
-    render plain: "I'm in the index action!"
+    render json: User.all
   end
 
   def create
-    render json: params
+    user = User.new(user_params)
+
+    if user.save
+      render json: user
+    else
+      render json: user.errors.full_messages, status: :unprocessable_entity
+    end
   end
 
   def show
     render json: params
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email)
   end
 end
