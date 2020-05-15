88f13f6ad469bea2085626793c567fca2eb20666
Adrian Ang <adriancbang@gmail.com>
2020-05-15 02:36:18 -0700

    Add show, update, and destroy controller actions

--- rails/routes_and_controllers/first_routes_and_controllers/app/controllers/users_controller.rb
+++ rails/routes_and_controllers/first_routes_and_controllers/app/controllers/users_controller.rb
@@ -14,7 +14,28 @@ class UsersController < ApplicationController
   end
 
   def show
-    render json: params
+    user = User.find_by(id: params[:id])
+    render json: user
+  end
+
+  def update
+    user = User.find_by(id: params[:id])
+
+    if user.update(user_params)
+      render json: user
+    else
+      render json: user.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    user = User.find_by(id: params[:id])
+
+    if user.destroy
+      render json: user
+    else
+      render json: "Cannot destroy this user"
+    end
   end
 
   private
