cf8317ccbda7d16a5521645cf2f89decc3576e30
Adrian Ang <adriancbang@gmail.com>
2020-05-17 02:56:43 -0700

    Create artworks table, with indices, validation, association with users table

--- rails/routes_and_controllers/art_share_api/db/seeds.rb
+++ rails/routes_and_controllers/art_share_api/db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+Artwork.destroy_all
+
+User.reset_pk_sequence
+Artwork.reset_pk_sequence
+
+user1 = User.create(username: "user1")
+user2 = User.create(username: "user2")
+
+artwork1 = Artwork.create(title: "title1", image_url: "img_url_1", artist_id: 1)
+artwork2 = Artwork.create(title: "title2", image_url: "img_url_2", artist_id: 1)
+artwork3 = Artwork.create(title: "title3", image_url: "img_url_3", artist_id: 2)
