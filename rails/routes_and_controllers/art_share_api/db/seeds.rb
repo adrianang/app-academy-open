61f41abc8847a38d0922bcbe1a20f964418761c4
Adrian Ang <adriancbang@gmail.com>
2020-05-17 18:43:53 -0700

    Add associations for ArtworkShare table, seed database with ArtworkShares

--- rails/routes_and_controllers/art_share_api/db/seeds.rb
+++ rails/routes_and_controllers/art_share_api/db/seeds.rb
@@ -8,9 +8,11 @@
 
 User.destroy_all
 Artwork.destroy_all
+ArtworkShare.destroy_all
 
 User.reset_pk_sequence
 Artwork.reset_pk_sequence
+ArtworkShare.reset_pk_sequence
 
 user1 = User.create(username: "user1")
 user2 = User.create(username: "user2")
@@ -18,3 +20,7 @@ user2 = User.create(username: "user2")
 artwork1 = Artwork.create(title: "title1", image_url: "img_url_1", artist_id: 1)
 artwork2 = Artwork.create(title: "title2", image_url: "img_url_2", artist_id: 1)
 artwork3 = Artwork.create(title: "title3", image_url: "img_url_3", artist_id: 2)
+
+artwork_share1 = ArtworkShare.create(artwork_id: 2, viewer_id: 2)
+artwork_share2 = ArtworkShare.create(artwork_id: 1, viewer_id: 2)
+artwork_share3 = ArtworkShare.create(artwork_id: 3, viewer_id: 1)
