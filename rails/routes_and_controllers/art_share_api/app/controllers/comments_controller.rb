e66d4841e79814b6f903520a7cd0c9e048c8fe0a
Adrian Ang <adriancbang@gmail.com>
2020-05-19 23:32:07 -0700

    Create comments controller and routes

---
+++ rails/routes_and_controllers/art_share_api/app/controllers/comments_controller.rb
@@ -0,0 +1,38 @@
+class CommentsController < ApplicationController
+  def index
+   if params[:user_id] && params[:artwork_id]
+      render json: Comment.where({ user_id: params[:user_id], artwork_id: params[:artwork_id] }) 
+   elsif params[:user_id] 
+      render json: Comment.where(user_id: params[:user_id])
+    elsif params[:artwork_id]
+      render json: Comment.where(artwork_id: params[:artwork_id])
+    else
+      render json: Comment.all
+    end
+  end
+
+  def create
+    comment = Comment.new(comment_params)
+
+    if comment.save
+      render json: comment
+    else
+      render json: comment.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    comment = Comment.find_by(id: params[:id])
+
+    if comment.destroy
+      render json: comment
+    else
+      render json: comment.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comments).permit(:user_id, :artwork_id, :body)
+  end
+end
