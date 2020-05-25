be47ae2d8e2173fa4fe0e8318e412f5deab95e45
Adrian Ang <adriancbang@gmail.com>
2020-05-24 17:42:37 -0700

    Initial commit, create Cats table and Cat model

---
+++ rails/rails_views/my_99cats/app/models/cat.rb
@@ -0,0 +1,13 @@
+class Cat < ApplicationRecord
+  include ActionView::Helpers::DateHelper
+  CAT_COLORS = %w(black brown orange white)
+
+  validates :birth_date, :color, :name, :sex, presence: true
+  validates :color, inclusion: { in: CAT_COLORS }
+  validates :sex, length: { maximum: 1 }, inclusion: { in: %w(M F) }
+
+  def age
+    age_from_today = Date.today - birth_date
+    time_ago_in_words(age_from_today.days.ago)
+  end
+end
