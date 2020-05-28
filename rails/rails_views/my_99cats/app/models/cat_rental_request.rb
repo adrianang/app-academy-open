b6d1b72325dcb816c099c3496fbbe247d301b482
Adrian Ang <adriancbang@gmail.com>
2020-05-28 02:15:09 -0700

    Add #approve! and #deny! methods to CatRentalRequest

--- rails/rails_views/my_99cats/app/models/cat_rental_request.rb
+++ rails/rails_views/my_99cats/app/models/cat_rental_request.rb
@@ -11,14 +11,30 @@ class CatRentalRequest < ApplicationRecord
   def overlapping_requests
     CatRentalRequest.where.not('start_date > ? OR end_date < ?', end_date, start_date)
                     .where.not('id = ?', id)
+                    .where('cat_id = ?', cat_id)
   end
 
   def overlapping_approved_requests
     self.overlapping_requests.where('status = ?', "APPROVED")
   end
 
+  def overlapping_pending_requests
+    self.overlapping_requests.where('status = ?', "PENDING")
+  end
+
   def does_not_overlap_approved_request
     self.overlapping_approved_requests.exists? ? false : true
+  end
+
+  def approve!
+    CatRentalRequest.transaction do
+      self.update(status: "APPROVED")
+      self.overlapping_pending_requests.update_all(status: "DENIED")
+    end
+  end
+
+  def deny!
+    self.update(status: "DENIED")
   end
 
   def end_date_cannot_be_before_start_date
