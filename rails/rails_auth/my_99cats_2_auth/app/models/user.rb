890f957953e05a8f022ad14aff93443a2f565d0b
Adrian Ang <adriancbang@gmail.com>
2020-06-11 00:55:51 -0700

    Add routes and controllers to Users, Session

--- rails/rails_auth/my_99cats_2_auth/app/models/user.rb
+++ rails/rails_auth/my_99cats_2_auth/app/models/user.rb
@@ -1,14 +1,15 @@
 class User < ApplicationRecord
   attr_reader :password
 
-  validates :user_name, :password_digest, presence: true
-  validates :session_token, presence: { message: 'Password can\'t be blank' }
+  validates :user_name, presence: true
+  validates :session_token, presence: true, uniqueness: true
+  validates :password_digest, presence: { message: 'Password can\'t be blank' }
   validates :password, length: { minimum: 6 }, allow_nil: true
 
-  after_validation :ensure_session_token
+  after_initialize :ensure_session_token
 
   def self.find_by_credentials(user_name, password)
-    user = self.class.find_by(user_name: user_name)
+    user = User.find_by(user_name: user_name)
     return nil if user.nil?
     user.is_password?(password) ? user : nil
   end
@@ -18,14 +19,14 @@ class User < ApplicationRecord
   end
 
   def reset_session_token!
-    self.session_token = self.class.generate_session_token
+    self.session_token = User.generate_session_token
     self.save!
     self.session_token
   end
 
   def password=(password)
     @password = password
-    self.password_digest = BCrypt::Password.create(password)
+    self.password_digest = BCrypt::Password.create(@password)
   end
 
   def is_password?(password)
@@ -34,6 +35,6 @@ class User < ApplicationRecord
 
   private
   def ensure_session_token
-    self.session_token ||= self.class.generate_session_token
+    self.session_token ||= User.generate_session_token
   end
 end
